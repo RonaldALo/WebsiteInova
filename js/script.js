e421415dbe3f69930661d5e27b3fd823cdfa1404
Ronald Alves <ronald.alves7@outlook.com>
2022-01-21 17:47:16 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    grabCursor: true,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
