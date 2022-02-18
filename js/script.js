3fea2dd5d09173a8aaeea800256ec0cdbc6cf634
Ronald Alves <ronald.alves7@outlook.com>
2022-02-18 15:03:16 -0300

    ajuste feito

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 // sliderShow 
 const swiper = new Swiper(".home-slider", {
-    spaceBetween: 30,
-    centeredSlides: true,
     grabCursor: true,
-    effect: "fade",
     autoplay: {
         delay: 2500,
         disableOnInteraction: false,
     },
     pagination: {
         el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        dynamicBullets: true,
     },
 });
